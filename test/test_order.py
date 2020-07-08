52296d5f863377bb27aa4ee4a6edeb0225ad8b57
nicolasraffray <nraffray@gmail.com>
2020-07-08 22:04:03 +0100

    refactored class back in test

--- test/test_order.py
+++ test/test_order.py
@@ -3,11 +3,13 @@ from ..lib.order import Order
 from ..lib.menu import Menu
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def order():
     order = Order(Menu({"Tofu": 2.50, "Fries": 1.95}))
     return order
 
 
-def test_inherits_menu(order):
-    assert isinstance(order.menu, Menu)
+class TestOrder:
+
+    def test_inherits_menu(self, order):
+        assert isinstance(order.menu, Menu)
